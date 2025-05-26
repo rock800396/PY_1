1c726376a21413a7d47a463b1fbf93d1ffacba4d
天上人间一号妹妹 <ydp19830624@gmail.com>
2025-05-27 00:46:30 +0800

    2025.5.27 00:46

--- pack_01/__init__.py
+++ pack_01/__init__.py
@@ -1,8 +1,8 @@
 print("看到这个提示,说明你执行到了__init__.py文件!")
 
-# 第一种方式:
+# 第一种方法:
 # from. import login # 导入登录模块
 # from. import register # 导入注册模块
 
-# 第二种方式:
+# 第二种方法:
 __all__ = ['login', 'register']  # 定义包的公开接口,只导出login和register模块
