31f5488b25757b4dc00160a2b2ada7ecd6aa096a
天上人间一号妹妹 <ydp19830624@gmail.com>
2025-06-26 01:10:46 +0800

    引用网页查询功能到本地

--- query_tool/query_tool.py
+++ query_tool/query_tool.py
@@ -1,6 +1,6 @@
 import requests
 from lxml import etree
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 def query_mobile(phone_num):
     url = f"https://www.ip138.com/mobile.asp?mobile={phone_num}&action=mobile"
@@ -16,7 +16,18 @@ def query_mobile(phone_num):
     list_msg = list_msg_2[0:1]+list_msg_1+ list_msg_2[1:3]
     return list_msg
 
-if __name__ == "__main__":
-    phone_num = input("请输入手机号码:")
-    list_msg = query_mobile(phone_num)
-    print(f"查询结果: {list_msg}")
+app = Flask(__name__)
+
+# 这是主页入口,访问"http://127.0.0.1:5000"时会渲染index.html模板
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/query")
+def query():
+    # 注意,这里的request不是requests库的request,而是Flask框架提供的请求对象
+    phone_num = request.args.get('phone_num')
+    # query_mobile(phone_num)返回的是一个列表,需要将其转换为字符串
+    return "<br/>".join(query_mobile(phone_num))
+
+app.run(debug=True)
